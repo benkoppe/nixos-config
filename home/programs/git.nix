f1dc623e5488906261a1203109c7addd8957d858
Ben Koppe <koppe.development@gmail.com>
2025-05-27 06:25:17 -0700

    Add additional git config
    - Default branch
    - Auto setup remote and rebase
    - Always use ssh instead of https

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,5 +6,21 @@
 
   programs.git = {
     enable = true;
+
+    userName = "Ben Koppe";
+    userEmail = "koppe.development@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase =  true;
+
+      # replace https with ssh
+      url = {
+        "ssh://git@github.com/benkoppe" = {
+          insteadOf = "https://github.com/benkoppe";
+        };
+      };
+    };
   };
 }
