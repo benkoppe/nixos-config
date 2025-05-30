46ff2c383076620cd2c78670c6ccc36d7d883ff1
Ben Koppe <koppe.development@gmail.com>
2025-05-30 11:49:16 -0700

    Update zsh home config

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -2,6 +2,60 @@
 {
   programs.zsh = {
     enable = true;
-    initContent = builtins.readFile "${dotfiles}/.config/zsh/.zshrc";
+
+    antidote = {
+      enable = true;
+      plugins = [''
+        getantidote/use-omz
+        ohmyzsh/ohmyzsh path:lib/git.zsh
+        ohmyzsh/ohmyzsh path:plugins/thefuck
+
+        zdharma-continuum/fast-syntax-highlighting kind:defer
+        zsh-users/zsh-history-substring-search
+        zsh-users/zsh-autosuggestions
+        unixorn/jpb.zshplugin
+        unixorn/warhol.plugin.zsh
+        unixorn/tumult.plugin.zsh
+        eventi/noreallyjustfuckingstopalready
+        djui/alias-tips
+        unixorn/fzf-zsh-plugin
+        chrissicool/zsh-256color
+        srijanshetty/docker-zsh
+        zsh-users/zsh-completions kind:fpath path:src
+        romkatv/powerlevel10k kind:fpath
+        zdharma-continuum/history-search-multi-word
+        trystan2k/zsh-tab-title
+      ''];
+    };
+
+    plugins = [
+      {
+        name = "aliases";
+        src = "${dotfiles}/.config/zsh";
+        file = "aliases.zsh";
+      }
+      {
+        name = "completion";
+        src = "${dotfiles}/.config/zsh";
+        file = "completion.zsh";
+      }
+      {
+        name = "keybinds";
+        src = "${dotfiles}/.config/zsh";
+        file = "keybinds.zsh";
+      }
+      {
+        name = "config";
+        src = "${dotfiles}/.config/zsh";
+        file = "config.zsh";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = "${dotfiles}/.config/zsh";
+        file = ".p10k.zsh";
+      }
+    ];
+
+    initContent = "autoload -Uz promptinit && promptinit && prompt powerlevel10k\nfastfetch";
   };
 }
