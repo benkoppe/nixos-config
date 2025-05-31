bab32399d5cce926bb09347dae158e99f33fe48c
Ben Koppe <koppe.development@gmail.com>
2025-05-31 02:14:44 -0700

    Add prettier explicit config

--- modules/common/neovim/lua/plugins/extras.lua
+++ modules/common/neovim/lua/plugins/extras.lua
@@ -21,8 +21,44 @@ return {
     opts = {
       servers = {
         nixd = {},
+        nil_ls = false,
+        lua_ls = {},
       },
     },
+  },
+
+  -- use prettierd
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      local supported = {
+        "css",
+        "graphql",
+        "handlebars",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "less",
+        "markdown",
+        "markdown.mdx",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "yaml",
+      }
+      for _, ft in ipairs(supported) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "prettierd")
+      end
+
+      opts.formatters = opts.formatters or {}
+      opts.formatters.prettier = {}
+      opts.formatters.prettierd = {}
+    end,
   },
 
   -- changing behavior of cmp scrolling
