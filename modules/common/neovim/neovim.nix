0396f3f1938bde7303b90426ca463ae733745995
Ben Koppe <koppe.development@gmail.com>
2025-05-31 00:22:48 -0700

    Format and start adding LSPs

--- modules/common/neovim/neovim.nix
+++ modules/common/neovim/neovim.nix
@@ -1,29 +1,53 @@
-{ inputs, config, lib, pkgs, dotfiles, ... }: let
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
   inherit (lib) enabled merge mkIf;
-in merge <| mkIf config.isDesktop {
-  home-manager.sharedModules = [{
-    imports = [ inputs.mnw.homeManagerModules.default ];
+in
+merge
+<| mkIf config.isDesktop {
+  home-manager.sharedModules = [
+    {
+      imports = [ inputs.mnw.homeManagerModules.default ];
 
-    programs.mnw = enabled {
-      luaFiles = [ ./init.lua ];
+      programs.mnw = enabled {
+        luaFiles = [ ./init.lua ];
 
-      extraBinPath = [
-	      pkgs.ripgrep
-	      pkgs.fzf
-        pkgs.stylua
-      ];
+        extraBinPath = [
+          pkgs.ripgrep
+          pkgs.fzf
 
-      plugins = {
-	start = [
-	  pkgs.vimPlugins.lazy-nvim
-	  pkgs.vimPlugins.plenary-nvim
-	];
+          # LSPs
+          pkgs.stylua
+          pkgs.nixd # nix lsp
+          pkgs.nixfmt-rfc-style
+          pkgs.prettierd
+        ];
 
-	dev.myconfig = {
-	  pure = ./.;
-	  impure = "/home/test/.nixos-config/modules/common/neovim";
-	};
+        plugins = {
+          start = [
+            pkgs.vimPlugins.lazy-nvim
+            pkgs.vimPlugins.plenary-nvim
+          ];
+
+          dev.myconfig = {
+            pure = ./.;
+            impure = "/home/test/.nixos-config/modules/common/neovim";
+          };
+        };
+
+        providers = {
+          nodeJs = enabled;
+          perl = enabled;
+          python3 = enabled;
+          ruby = enabled;
+
+        };
       };
-    };
-  }];
+    }
+  ];
 }
