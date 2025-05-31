307aeecab1ac81c4f3b05bf304af89928c50397e
Ben Koppe <koppe.development@gmail.com>
2025-05-31 13:26:38 -0700

    Feat: add extensions

--- modules/common/browsers/chromium.nix
+++ modules/common/browsers/chromium.nix
@@ -1,12 +1,22 @@
 { config, lib, ... }:
 let
   inherit (lib) enabled merge mkIf;
+  extensions = [
+    { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+    { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # sponsorblock
+    { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # vimium
+    { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # dark reader
+    { id = "pejdijmoenmkgeppbflobdenhhabjlaj"; } # icloud passwords
+    { id = "aapbdbdomjkkjkaonfhkkikfgjllcleb"; } # google translate
+  ];
 in
 merge
 <| mkIf config.isDesktop {
   home-manager.sharedModules = [
     {
-      programs.chromium = enabled;
+      programs.chromium = enabled {
+        inherit extensions;
+      };
     }
   ];
 }
