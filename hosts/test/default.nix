8c68bbf86f82e5630839b27039d16d163b1e95e3
Ben Koppe <koppe.development@gmail.com>
2025-06-06 05:08:35 -0700

    Use open nvidia drivers

--- hosts/test/default.nix
+++ hosts/test/default.nix
@@ -61,6 +61,7 @@ lib.nixosSystem' (
     services.spice-vdagentd.enable = true;
 
     hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    hardware.nvidia.open = true;
     services.xserver.videoDrivers = [ "nvidia" ];
   }
 )
