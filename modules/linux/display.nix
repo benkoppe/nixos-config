41fbfe38b6c61be47c9442e181f7f96d6e520f7f
Ben Koppe <koppe.development@gmail.com>
2025-06-08 14:02:32 -0700

    Combine linux display settings

--- modules/linux/display.nix
+++ modules/linux/display.nix
@@ -4,6 +4,12 @@ let
 in
 merge
 <| mkIf config.isDesktop {
+  # use sddm as displayManager
+  services.displayManager = {
+    sddm = enabled;
+    defaultSession = "none+i3";
+  };
+
   # Configure keymap in X11
   services.xserver = enabled {
     xkb = {
