ceb1d8bfe3fbd32a1ca4d6475f5e87f219591b6b
Ben Koppe <koppe.development@gmail.com>
2025-06-08 14:06:22 -0700

    Reformat merge style in display.nix

--- modules/linux/display.nix
+++ modules/linux/display.nix
@@ -3,36 +3,38 @@ let
   inherit (lib) enabled mkIf merge;
 in
 merge
-<| mkIf config.isDesktop {
-  # use sddm as displayManager
-  services.displayManager = {
-    sddm = enabled;
-    defaultSession = "none+i3";
-  };
-
-  # Configure keymap in X11
-  services.xserver = enabled {
-    xkb = {
-      layout = "us";
-      variant = "";
+  (mkIf config.isDesktop {
+    # use sddm as displayManager
+    services.displayManager = {
+      sddm = enabled;
+      defaultSession = "none+i3";
     };
-  };
 
-  # Configure inputs
-  services.libinput = enabled {
-    mouse = {
-      accelProfile = "flat";
+    # Configure keymap in X11
+    services.xserver = enabled {
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
     };
-  };
 
-  # Enable auto login
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.autoLogin.user = "test";
+    # Configure inputs
+    services.libinput = enabled {
+      mouse = {
+        accelProfile = "flat";
+      };
+    };
 
-}
-<| mkIf config.isVM {
-  # Add qemu-guest-agent
-  services.qemuGuest.enable = true;
-  # Add spice-vdagent
-  services.spice-vdagentd.enable = true;
-}
+    # Enable auto login
+    # services.displayManager.autoLogin.enable = true;
+    # services.displayManager.autoLogin.user = "test";
+
+  })
+  (
+    mkIf config.isVM {
+      # Add qemu-guest-agent
+      services.qemuGuest.enable = true;
+      # Add spice-vdagent
+      services.spice-vdagentd.enable = true;
+    }
+  )
