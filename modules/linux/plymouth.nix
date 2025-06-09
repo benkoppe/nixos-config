c32c237fec0292a53af8961e179515ee1571cf85
Ben Koppe <koppe.development@gmail.com>
2025-06-08 18:39:11 -0700

    Enable silent boot

--- modules/linux/plymouth.nix
+++ modules/linux/plymouth.nix
@@ -8,6 +8,22 @@ let
 in
 merge (
   mkIf config.isDesktop {
-    boot.plymouth = enabled;
+    boot = {
+      plymouth = enabled;
+
+      # enable "silent boot"
+      consoleLogLevel = 3;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+      # hide the OS choice for bootloaders.
+      # requires a keypress to appear
+      loader.timeout = 0;
+    };
   }
 )
