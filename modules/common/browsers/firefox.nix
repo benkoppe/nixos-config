e751097c254caa72f7931e2edd02dc6a40b7763f
Ben Koppe <koppe.development@gmail.com>
2025-06-09 12:51:59 -0700

    Add firefox to browsers

---
+++ modules/common/browsers/firefox.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) merge mkIf enabled;
+in
+merge
+<| mkIf (config.isDesktop) {
+  home-manager.sharedModules = [
+    {
+      programs.firefox = enabled {
+        policies = {
+          # Check about:policies#documentation for options.
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          DisablePocket = true;
+          DisableFirefoxAccounts = true;
+          DisableAccounts = true;
+          DisableFirefoxScreenshots = true;
+          OverrideFirestRunPage = "";
+          OverridePostUpdatePage = "";
+          DontCheckDefaultBrowser = true;
+          DisplayBookmarksToolbar = "always"; # alternatives: "always" or "newtab"
+          DisplayMenuBar = "default-off"; # alternatives: "always", "never", or "default-on"
+          SearchBar = "unified";
+        };
+
+        profiles = {
+          default = {
+            id = 0;
+            name = "default";
+            isDefault = true;
+            extensions = with pkgs.nur.repos.rycee.firefox.addons; [
+              ublock-origin
+              darkreader
+              vimium
+            ];
+          };
+        };
+      };
+    }
+  ];
+}
