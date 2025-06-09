f58aabe036da95550a25e8224ab59ae9a3edce62
Ben Koppe <koppe.development@gmail.com>
2025-06-09 09:52:29 -0700

    Remove xsession-wrapper flag

--- modules/linux/tuigreet.nix
+++ modules/linux/tuigreet.nix
@@ -13,7 +13,7 @@ merge (
     services.greetd = enabled {
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --xsessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session --xsession-wrapper";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --xsessions ${config.services.xserver.displayManager.sessionData.desktops}/share/xsessions --sessions ${config.services.xserver.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
           user = "greeter";
         };
       };
