4a85f8f5dbc602838df66542012b5e1019ce1041
Ben Koppe <koppe.development@gmail.com>
2025-06-08 18:01:08 -0700

    Create dedicated sddm file for theming

--- modules/linux/display.nix
+++ modules/linux/display.nix
@@ -10,12 +10,6 @@ let
 in
 merge
   (mkIf config.isDesktop {
-    # use sddm as displayManager
-    services.displayManager = {
-      sddm = enabled;
-      defaultSession = "none+i3";
-    };
-
     # Configure keymap in X11
     services.xserver = enabled {
       xkb = {
