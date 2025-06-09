e997e3ca1f79c3f8561567d38000c8e273ee330b
Ben Koppe <koppe.development@gmail.com>
2025-06-08 19:35:12 -0700

    Use home manager modules for sops-nix

--- modules/common/sops-nix.nix
+++ modules/common/sops-nix.nix
@@ -1,3 +1,7 @@
 { sops-nix, ... }:
-sops-nix.nixosModules.sops
+{
+  home-manager.sharedModules = [
+    sops-nix.homeManagerModules.sops
+  ];
+}
 # TODO: right now this uses `nixos` and is thus linux-only. For darwin, we'll need a switch
